664058c80617226fc1bf93ba51f5ab633f2d9571
hungryrussian <153167271+hungryrussian@users.noreply.github.com>
2023-12-07 15:04:02 -0800

    Add files via upload

---
+++ Delete-CachedFoxitInstallers.ps1
@@ -0,0 +1,17 @@
+# hungryrussian
+# 6/2023
+# Recurse through rdsusersfolders and delete cached FoxIt Installers
+# To test, run script as is
+# To execute delete, uncomment last line
+
+$rdsUsersFolders = Get-ChildItem -Path "S:\rdsusersfolders" -Directory
+$foxitPath = "\AppData\Roaming\Foxit Software\Addon\Foxit Reader\Install"
+
+foreach($folder in $rdsUsersFolders) {
+    $userFoxitPath = "S:\rdsusersfolders\" + $folder.BaseName + $foxitPath
+    $result = Test-Path $userFoxitPath
+    if($result -eq $True) {
+        Get-ChildItem -Path $userFoxitPath -Recurse | Remove-Item -Force -Recurse -Whatif
+        #Get-ChildItem -Path $userFoxitPath -Recurse | Remove-Item -Force -Recurse
+    }
+}
